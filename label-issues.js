9c262f5283c548a502800a709a23096a822b8d6e
gshivi <123095952+gshivi@users.noreply.github.com>
2023-06-27 12:36:54 +0530

    Update label-issues.js

--- label-issues.js
+++ label-issues.js
@@ -2,15 +2,16 @@ const { Octokit } = require('@octokit/rest');
 
 async function main() {
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
+  console.log(process.env.GITHUB_TOKEN);
+  console.log(process.env.GITHUB_ISSUE_NUMBER);
+  console.log("hello");
   // Fetch the issue information
   const issue = await octokit.issues.get({
     owner: 'gshivi',
     repo: 'Code-Phoenix',
     issue_number: process.env.GITHUB_ISSUE_NUMBER
   });
-  console.log(process.env.GITHUB_TOKEN);
-  console.log(process.env.GITHUB_ISSUE_NUMBER);
+  
   const label = ['label1', 'label2', 'label3'] ;
 
   // Add the label to the issue
