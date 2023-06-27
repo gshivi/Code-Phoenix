1af7e224accb473f92302713a69a5de7cb209f8c
gshivi <123095952+gshivi@users.noreply.github.com>
2023-06-27 14:52:49 +0530

    Update label-issues.js

--- label-issues.js
+++ label-issues.js
@@ -19,13 +19,26 @@ async function main() {
   const label = ['label1', 'label2', 'label3'] ;
 
   // Add the label to the issue
-  await octokit.issues.update({
-    owner: process.env.GITHUB_REPOSITORY_OWNER,
-    repo: process.env.GITHUB_REPOSITORY_NAME,
-    issue_number: process.env.GITHUB_ISSUE_NUMBER,
-    labels: label,
-  });
-}
+//   await octokit.issues.update({
+//     owner: process.env.GITHUB_REPOSITORY_OWNER,
+//     repo: process.env.GITHUB_REPOSITORY_NAME,
+//     issue_number: process.env.GITHUB_ISSUE_NUMBER,
+//     labels: label,
+//   });
+// }
+
+await octokit.request('POST /repos/{process.env.GITHUB_REPOSITORY_OWNER}/{process.env.GITHUB_REPOSITORY_NAME}/issues/{process.env.GITHUB_ISSUE_NUMBER}/labels', {
+  owner: 'process.env.GITHUB_REPOSITORY_OWNER',
+  repo: 'process.env.GITHUB_REPOSITORY_NAME',
+  issue_number: 'process.env.GITHUB_ISSUE_NUMBER',
+  labels: [
+    'bug',
+    'enhancement'
+  ],
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
 
 main().catch((error) => {
   console.error('Error:', error);
