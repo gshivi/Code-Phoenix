8745b6933ad99c38fe08f2fdb05b34988ca11c9e
Shivika Gupta <shivikagupta@microsoft.com>
2023-06-27 20:37:13 +0530

    sending gpt label in payload3

--- label-issues.js
+++ label-issues.js
@@ -13,7 +13,6 @@ async function main() {
   });
 
   const issueTitle = issue.data.title;
-  const issueBody = issue.data.body;
   const prompt1 = `You are a GitHub automation tool and your primary purpose is to generate labels based on the issue title. Issue title:${issueTitle}. Return a label in the JSON format {label: labelName} refer to the following examples to determine the response format. If the label is paportal return {label: paportal}. If the label is pcf return {label: pcf}. If the label is canvasApps return {label: canvasApps}.`;
   
   let data = JSON.stringify({
@@ -37,51 +36,27 @@ async function main() {
     },
     data : data
   };
- // let label = '';
+
   axios.request(config)
   .then((response) => {
-    //console.log(JSON.stringify(response.data));
     const gptResponse = response.data.choices[0].text;
     const labelIndex = gptResponse.indexOf('label: ');
     const labelValueStartIndex = labelIndex + 7;
     const labelValueEndIndex = gptResponse.indexOf('}', labelValueStartIndex);
     const labelValue = gptResponse.substring(labelValueStartIndex, labelValueEndIndex).trim();
-
+    console.log([labelValue])
     // Add the label to the issue
     const res = octokit.issues.addLabels({
-    owner: process.env.GITHUB_REPOSITORY_OWNER,
-    repo: process.env.GITHUB_REPOSITORY_NAME,
-    issue_number: process.env.GITHUB_ISSUE_NUMBER,
-    labels: [labelValue],
-  });  
+      owner: process.env.GITHUB_REPOSITORY_OWNER,
+      repo: process.env.GITHUB_REPOSITORY_NAME,
+      issue_number: process.env.GITHUB_ISSUE_NUMBER,
+      labels: [labelValue],
+    });  
 
-    // console.log(labelValue);
-    // console.log([labelValue])
-    // console.log("GPTResponse" + gptResponse);
-    // const gptstring = JSON.stringify(gptResponse);
-    // console.log(gptstring);
-    // //const jsonObject = JSON.parse(response.data);
-    // console.log("Choices");
-    // console.log(response.data.choices[0]);
-    // console.log("Choices text");
-    // console.log(response.data.choices[0].text);
-    // label = response.data.choices[0].text.label;
-
-    // console.log("Label");
-    // console.log(label);
-    // console.log([label])
-    // jsonObject.choices[0].text = '\n\n{label: NewLabelValue}'; // Assign the new value to the "label" property
-    // const updatedJsonString = JSON.stringify(jsonObject);
-
-    
   })
   .catch((error) => {
     console.log(error);
-  });
-  
-  //const label = ['label1', 'label2', 'label3'] ;
-
-  
+  }); 
 }
 
 main().catch((error) => {
